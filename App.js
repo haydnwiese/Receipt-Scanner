1ffa12f90c7b869e7bde79da92887879d04daeea
haydnwiese <45501692+haydnwiese@users.noreply.github.com>
2019-09-14 13:40:30 -0400

    Creating CameraView

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TextDetection from './Components/TextDetection';
+import CameraView from './Components/CameraView';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <CameraView />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
